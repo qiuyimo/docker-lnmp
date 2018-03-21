05e412a1514b035be8755d8ae0374c2ec776491d
qiuyu <530004000@qq.com>
2018-03-21 15:14:48 +0800

    docker, mysql

--- app/localhost/index.php
+++ app/localhost/index.php
@@ -6,4 +6,13 @@
  * Time: 下午12:02
  */
 
+$dbh = new PDO("mysql:host=mysql;dbname=test", 'root', 'root');
+if ($dbh) {
+    echo 'Connect MySQL access';
+} else {
+    echo 'Connect MySQL error';
+}
+
+echo "<hr/>";
+
 echo phpinfo();
